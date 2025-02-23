622fcfbd3c706437bde429f23a09d41d27b05b5e
ivansaul <saul_2503@hotmail.com>
2025-02-23 00:10:04 -0500

    initial commit

---
+++ Wordly/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Wordly
+//
+//  Created by @ivansaul on 2/22/25.
+//
+//  https://github.com/ivansaul
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Wordly/WordlyApp.swift
@@ -0,0 +1,19 @@
+//
+//  WordlyApp.swift
+//  Wordly
+//
+//  Created by @ivansaul on 2/22/25.
+//
+//  https://github.com/ivansaul
+//
+
+import SwiftUI
+
+@main
+struct WordlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
